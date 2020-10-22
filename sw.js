53349e765bd519dd7a5436062c78d07286974847
Francesco Montresor <mon3fran@gmail.com>
2020-10-22 18:00:34 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,34 @@
-self.addEventListener('install', function(event) {
-    console.log('[Service Worker] Installing Service Worker ...', event);
+const version = 0
+
+self.addEventListener('install', function (event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-  caches.open('static').then(function(cache) {
-   cache.addAll(['/index.html', '/manifest.webmanifest']);
-    })
-    );
-  });
-  self.addEventListener('activate', function(event) {
-   console.log('[Service Worker] Activating Service Worker ....', event);
-  });
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-  caches.match(event.request).then(function(response) {
-    if (response) {
-      return response;
-    } else {
-      return fetch(event.request).then(function(res) {
-        return caches.open('dynamic').then(function(cache) {
-          cache.put(event.request.url, res.clone());
-          return res;
+    caches.keys().then(
+      cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          console.log('deleting cache: '+cacheName)
+          return caches.delete(cacheName);
+        }))
+      }).then( () => self.skipWaiting())
+  );
+});
+self.addEventListener('activate', function (event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+});
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request).then(function (res) {
+          return caches.open('dynamic').then(function (cache) {
+            cache.put(event.request.url, res.clone());
+            return res;
+          });
         });
-      });
-    }
-  })
-    );
-  });
+      }
+    })
+  );
+});
